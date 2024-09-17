7f64dea6aea6a2d70d6d497217411302b65fdc77
ingridwarnesen <ingrwar@stud.ntnu.no>
2024-09-17 11:13:48 +0200

    related to task 1

--- exercise_0_battery_in_the_reference_system.py
+++ exercise_0_battery_in_the_reference_system.py
@@ -26,9 +26,9 @@ import math
 # Location of (processed) data set for CINELDI MV reference system
 # (to be replaced by your own local data folder)
 #path_data_set         = 'C:/Users/ivespe/Data_sets/CINELDI_MV_reference_system/'
-#path_data_set = "/Users/ingridwiig/Documents/NTNU/5. klasse/Modul3/CINELDI_MV_reference_system_v_2023-03-06/"
+path_data_set = "/Users/ingridwiig/Documents/NTNU/5. klasse/Modul3/CINELDI_MV_reference_system_v_2023-03-06/"
 #path_data_set = "/Users/andreamarie/Documents/Blokk 3 - fordypningsemne fleksibilitet /Oving 0/CINELDI_MV_reference_system_v_2023-03-06/"
-path_data_set = "C:/Users/haral/CINELDI_MV_reference_system_v_2023-03-06/"
+#path_data_set = "C:/Users/haral/Fordypningsemne_modul3/CINELDI_MV_reference_system_v_2023-03-06/"
 
 filename_residential_fullpath = os.path.join(path_data_set,'time_series_IDs_primarily_residential.csv')
 filename_irregular_fullpath = os.path.join(path_data_set,'time_series_IDs_irregular.csv')      
@@ -36,16 +36,19 @@ filename_load_data_fullpath = os.path.join(path_data_set,'load_data_CINELDI_MV_r
 filename_load_mapping_fullpath = os.path.join(path_data_set,'mapping_loads_to_CINELDI_MV_reference_grid.csv')
 
 # %% Read pandapower network
-
 net = ppcsv.read_net_from_csv(path_data_set, baseMVA=10)
-pf=0.95
-p=1
-q=p*math.tan(math.acos(pf))
 
-pp.create_load(net,bus=95,p_mw=1,q_mvar=0.5)
+#adding a load of 1MW at . positive values equals load
+p_mw = 1
+pf = 0.95
+q_mvar = p_mw * math.tan(math.acos(pf))
 
+pp.create_load(net, 95, p_mw, q_mvar, const_z_percent=0, const_i_percent=0)
 # %% Test running power flow with a peak load model
 # (i.e., all loads are assumed to be at their annual peak load simultaneously)
+
+#insterting battery
+#pp.create_load(net, 94, -p_mw*10, -q_mvar, const_z_percent=0, const_i_percent=0)
 
 pp.runpp(net,init='results',algorithm='bfsw')
 
